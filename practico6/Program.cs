322546e1c6b944ba9c5b9a10df30c3da23b93eca
José Alberto Varela <varelaj298@gmail.com>
2023-05-24 10:22:37 -0300

    modificacion

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -53,80 +53,79 @@ las 4 operaciones básicas (Sumar, Restar, Multiplicar y Dividir) a partir de un
 que luego pida dos números y que devuelva el resultado de la operación seleccionada. Además una vez que termine de realizar 
 la operación le pregunte si desea realizar otro cálculo.
 */
+// Declaracion de variables
 int opcion, num1, num2, resultado;
 string? numString;
-bool continuar = true;
+bool continuar = true, control1 = true, control2 = true;
 while (continuar)
 {
+    // Menú
+    do
+    {
+        Console.WriteLine("Elija una opción: \n1 - Sumar \n2 - Restar \n3 - Multiplicar \n4 - Dividir \n0 - Salir");
+        numString = Console.ReadLine();
+        continuar = int.TryParse(numString, out opcion);
+        switch (opcion)
+        {
+            case 1:
+                Console.WriteLine("Eligio la opcion 1 de Sumar 2 numeros enteros");
+                break;
+            case 2:
+                Console.WriteLine("Eligio la opcion 2 de Restar 2 numeros enteros");
+                break;
+            case 3:
+                Console.WriteLine("Eligio la opcion 3 de Multiplicar 2 numeros enteros");
+                break;
+            case 4:
+                Console.WriteLine("Eligio la opcion 4 de Dividir 2 numeros enteros");
+                break;
+            case 0:
+                Console.WriteLine("Eligio la opcion 0 de Salir");
+                break;
+            default:
+                Console.WriteLine("Opcion Invalida\n");
+                break;
+        }
+    } while (opcion < 0 || opcion > 4);
 
-    Console.WriteLine("ingrese la opcion a realizar");
-    Console.WriteLine("1 - Sumar");
-    Console.WriteLine("2 - Restar");
-    Console.WriteLine("3 - Multiplicar");
-    Console.WriteLine("4 - Dividir ('/')");
-    Console.WriteLine("5 - Dividir ('%')");
-    Console.WriteLine("0 - Salir");
 
+    Console.WriteLine("ingrese el primer numero:");
+    numString = Console.ReadLine();
+    control1 = int.TryParse(numString, out num1);
+    Console.WriteLine("ingrese el segundo numero:");
+    numString = Console.ReadLine();
+    control2 = int.TryParse(numString, out num2);
+    if (control1 && control2)
+    {
+        switch (opcion)
+        {
+            case 1:
+                resultado = num1 + num2;
+                Console.WriteLine("el resultado es: " + resultado);
+                break;
+            case 2:
+                resultado = num1 - num2;
+                Console.WriteLine("el resultado es: " + resultado);
+                break;
+            case 3:
+                resultado = num1 * num2;
+                Console.WriteLine("el resultado es: " + resultado);
+                break;
+            case 4:
+                resultado = num1 / num2;
+                Console.WriteLine("el resultado de la division entera es: " + resultado);
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("uno o los dos numeros ingresados no son numeros enteros");
+    }
+    Console.WriteLine("Desea Realizar otra Operacion? - Presione 0 para salir o 1 para relizar otra operacion");
     numString = Console.ReadLine();
     continuar = int.TryParse(numString, out opcion);
-
-    switch (opcion)
+    if (opcion == 0)
     {
-        case 1:
-            Console.WriteLine("ingrese el primer numero");
-            numString = Console.ReadLine();
-            continuar = int.TryParse(numString, out num1);
-            Console.WriteLine("ingrese el segundo numero");
-            numString = Console.ReadLine();
-            continuar = int.TryParse(numString, out num2);
-            resultado = num1 + num2;
-            Console.WriteLine("el resultado es: " + resultado);
-            break;
-        case 2:
-            Console.WriteLine("ingrese el primer numero");
-            numString = Console.ReadLine();
-            continuar = int.TryParse(numString, out num1);
-            Console.WriteLine("ingrese el segundo numero");
-            numString = Console.ReadLine();
-            continuar = int.TryParse(numString, out num2);
-            resultado = num1 - num2;
-            Console.WriteLine("el resultado es: " + resultado);
-            break;
-        case 3:
-            Console.WriteLine("ingrese el primer numero");
-            numString = Console.ReadLine();
-            continuar = int.TryParse(numString, out num1);
-            Console.WriteLine("ingrese el segundo numero");
-            numString = Console.ReadLine();
-            continuar = int.TryParse(numString, out num2);
-            resultado = num1 * num2;
-            Console.WriteLine("el resultado es: " + resultado);
-            break;
-        case 4:
-            Console.WriteLine("ingrese el primer numero");
-            numString = Console.ReadLine();
-            continuar = int.TryParse(numString, out num1);
-            Console.WriteLine("ingrese el segundo numero");
-            numString = Console.ReadLine();
-            continuar = int.TryParse(numString, out num2);
-            resultado = num1 / num2;
-            Console.WriteLine("el resultado es: " + resultado);
-            break;
-        case 5:
-            Console.WriteLine("ingrese el primer numero");
-            numString = Console.ReadLine();
-            continuar = int.TryParse(numString, out num1);
-            Console.WriteLine("ingrese el segundo numero");
-            numString = Console.ReadLine();
-            continuar = int.TryParse(numString, out num2);
-            resultado = num1 % num2;
-            Console.WriteLine("el resultado es: " + resultado);
-            break;
-        case 0:
-            continuar = false;
-            break;
+        continuar = false;
     }
-
 }
-
-
